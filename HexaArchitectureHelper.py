c01f90bb20dba42b726d76376d6deb72b90850cf
Bryan <bafricano@ginzo.tech>
2022-08-12 09:19:02 +0200

    feat: added some folders refactor: added spaces to improve the understandability of the code

--- HexaArchitectureHelper.py
+++ HexaArchitectureHelper.py
@@ -5,27 +5,41 @@ def create_hexa_arch(name: str, path: str):
     folder = path + "/" + name
     os.mkdir(folder)
     os.mkdir(folder + "/application/")
-    os.mkdir(folder + "/domain/")
-    os.mkdir(folder + "/application/ports/")
     os.mkdir(folder + "/infraestructure/")
-    os.mkdir(folder + "/infraestructure/adapters/")
-    os.mkdir(folder + "/infraestructure/adapters/input/")
-    os.mkdir(folder + "/infraestructure/adapters/output/")
-    os.mkdir(folder + "/infraestructure/adapters/input/rest/")
-    os.mkdir(folder + "/infraestructure/adapters/output/customizedException/")
-    os.mkdir(folder + "/infraestructure/adapters/output/customizedException/data/")
-    os.mkdir(folder + "/infraestructure/adapters/output/persistence/")
+    os.mkdir(folder + "/domain/")
+
+    os.mkdir(folder + "/application/ports/")
     os.mkdir(folder + "/application/ports/input")
     os.mkdir(folder + "/application/ports/output")
+
+    os.mkdir(folder + "/infraestructure/adapters/")
+
+    os.mkdir(folder + "/infraestructure/adapters/input/")
     os.mkdir(folder + "/infraestructure/adapters/config")
+    os.mkdir(folder + "/infraestructure/adapters/output/")   
+
+    os.mkdir(folder + "/infraestructure/adapters/input/rest/")
     os.mkdir(folder + "/infraestructure/adapters/input/eventListener")
+
     os.mkdir(folder + "/infraestructure/adapters/input/rest/data")
     os.mkdir(folder + "/infraestructure/adapters/input/rest/mapper")
+
+    os.mkdir(folder + "/infraestructure/adapters/input/rest/data/request")
+    os.mkdir(folder + "/infraestructure/adapters/input/rest/data/response")
+     
+    os.mkdir(folder + "/infraestructure/adapters/output/customizedException/")    
+    os.mkdir(folder + "/infraestructure/adapters/output/persistence/")   
+    os.mkdir(folder + "/infraestructure/adapters/output/eventPublisher/")
+
+    os.mkdir(folder + "/infraestructure/adapters/output/customizedException/data/")
     os.mkdir(folder + "/infraestructure/adapters/output/customizedException/data/response")
-    os.mkdir(folder + "/infraestructure/adapters/output/eventPublisher")
+
     os.mkdir(folder + "/infraestructure/adapters/output/persistence/dto")
     os.mkdir(folder + "/infraestructure/adapters/output/persistence/mapper")
-    os.mkdir(folder + "/infraestructure/adapters/output/persistence/repository")
+    os.mkdir(folder + "/infraestructure/adapters/output/persistence/repository")    
+    
+    os.mkdir(folder + "/infraestructure/adapters/output/customizedException/data/response")    
+    
     os.mkdir(folder + "/domain/event")
     os.mkdir(folder + "/domain/exception")
     os.mkdir(folder + "/domain/model")
